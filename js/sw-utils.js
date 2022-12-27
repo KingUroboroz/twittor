67acd4d1ea67e65b0f76ec78c46ec1cefc6c2529
hector.gonzalez <hector.gonzalez@exodus.com.mx>
2022-12-27 10:24:33 -0600

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+//Guarda en Dynamic Cache
+function actualizaCacheDinamico(dynamicCache, request, response) {
+    if(response.ok){
+        caches.open(dynamicCache)
+        .then(cache => {
+            cache.put(request,response.clone());
+            return response.clone();
+        })
+    }else{
+        return response;
+    }
+}
